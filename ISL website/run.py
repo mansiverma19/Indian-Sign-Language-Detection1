36d61c179ee3d13a5b093cc99cdec7e75b493384
Mansi Verma <83285383+mansiverma19@users.noreply.github.com>
2023-08-13 22:47:39 +0530

    model file

--- ISL website/run.py
+++ ISL website/run.py
@@ -1,10 +1,3 @@
-import Detect
-
-lst = ['1', '2', '3', '4','5', '6', '7', '8','9','A', 'B', 'C', 'D','E', 'F', 'G', 'H','I', 'J', 'K', 'L','M', 'N', 'O', 'P','Q','R', 'S', 'T', 'U','V', 'W', 'X', 'Y', 'Z']
-nm="centernet"
-filename='G.jpg'
-threshold=0.1
-detector = Detect.Detect()
-detector.class_color(lst)
-detector.load_model(nm)
-detector.predictImg(filename,lst,threshold)
+version https://git-lfs.github.com/spec/v1
+oid sha256:2849724bace8e81f7bd1fac79b4bf9f04466342c07f822b0b066fac2983b9193
+size 363
